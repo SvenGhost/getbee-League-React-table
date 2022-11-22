448e59c22b794cea4d54a18fd150739122211ec8
SvenGhost <peer.anywhere@gmail.com>
2022-11-22 10:38:17 +0100

    React Table for getbee League table

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from "./component/Header/Header"
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div className={style.welcomeMessage}>
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
